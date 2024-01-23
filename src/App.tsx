3f0cdb19c41d4ae47fb80e8aad6b899c9817c1a3
Hussain Ali <iamhussainali2@gmail.com>
2024-01-23 16:01:37 +0530

    added navbar and tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@material-tailwind/react";
+import { Toaster } from "react-hot-toast";
+import { NavbarDefault } from "./components/Navbar";
+import { TabsWithIcon } from "./components/Tabs";
 
 function App() {
 
   return (
     <>
-      <Button placeholder="click">Click here</Button>
+     <div className="container mx-auto">
+     <Button placeholder="click">Click here</Button>
+      <NavbarDefault/>
+      <TabsWithIcon/>
+      <Toaster/>
+     </div>
     </>
   );
 }
